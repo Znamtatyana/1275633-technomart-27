5d1f19e37eb5aea92ce944f8810b359c44055825
Znamtatyana <37051172+tanumi@users.noreply.github.com>
2020-03-18 21:42:58 +0300

    Исправление замечаний.

---
+++ js/popup_buy.js
@@ -0,0 +1,34 @@
+var linkBuys = document.querySelectorAll(".buy-link");
+var popupBuy = document.querySelector(".modal-buy");
+var linkConinue = popupBuy.querySelector(".continue-buy");
+var closeBuy = popupBuy.querySelector(".modal-close");
+
+for (var linkBuy of linkBuys) {
+  linkBuy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupBuy.classList.add("modal-buy-show");
+    popupBuy.classList.remove("modal-buy-show-close");
+  });
+}
+
+linkConinue.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  popupBuy.classList.remove("modal-buy-show");
+  popupBuy.classList.add("modal-buy-show-close");
+})
+
+closeBuy.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  popupBuy.classList.remove("modal-buy-show");
+  popupBuy.classList.add("modal-buy-show-close");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if(popupBuy.classList.contains("modal-buy-show")) {
+      evt.preventDefault();
+      popupBuy.classList.remove("modal-buy-show");
+      popupBuy.classList.add("modal-buy-show-close");
+    }
+  }
+});
